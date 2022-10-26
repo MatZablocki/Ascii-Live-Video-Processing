4d183f4a4c3a76997942d142db7b6f4cb677b512
MatZablocki <matzayt@gmail.com>
2022-10-26 20:02:28 +0100

    fin

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let video;
+let adciiDiv
+const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
 
+function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(48,48);
+  asciiDiv = createDiv();
+}
 function draw() {
-  background(220);
+  video.loadPixels();
+  let asciiImage = '';
+  for(let j=0;j<video.height;j++) {
+    for(let i=0;i<video.width;i++) {
+      const imgIndex = (i + j * video.width) *4;
+      const r = video.pixels[imgIndex];
+      const g = video.pixels[imgIndex+1];
+      const b = video.pixels[imgIndex+2];
+      const avg = (r+g+b)/3;
+      const ascii = floor(map(avg, 0,255,density.length,0));
+      const check =density.charAt(ascii);
+      if(check == ' ') {
+        asciiImage+='&nbsp;';
+      }
+      else {
+        asciiImage +=check;
+      }
+      
+    }
+    asciiImage+="<br/>"
+    asciiDiv.html(asciiImage);
+    // console.log(row);
+  }
 }
